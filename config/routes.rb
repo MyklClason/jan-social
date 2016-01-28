b39aef5e7e62ba80e11cc05efd354d0136ca2106
Mykl Clason <myklclason@gmail.com>
2016-01-28 13:35:22 +0000

    Add users index and show; found bug in displaying mesages in sidebar. disabled function for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
+
   resources :messages
   devise_for :users
+  resources :users, only: [:show, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
